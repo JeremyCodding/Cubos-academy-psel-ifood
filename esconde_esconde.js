c1de41a78cbc176a4cf7bea6b2a62b6656f3481e
JeremyCodding <jeremy.workplace@gmail.com>
2021-08-10 19:08:08 -0300

    Passar esse programa pra minha irmã

---
+++ esconde_esconde.js
@@ -0,0 +1,10 @@
+function solucao(numero) {
+	if(numero>20){
+        for(conta = 21; conta <= numero; conta++){
+            console.log(conta);
+        }
+    }else{
+        console.log('VOCE CONSEGUE, MANINHO')
+    }
+    
+}
